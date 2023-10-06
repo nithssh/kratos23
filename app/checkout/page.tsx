a62957fb8884437656adb240ee475e40e8d35c74
rohithc123 <rohithc.eec.cseb@gmail.com>
2023-10-06 14:17:29 +0530

    completed basic ui your bag

---
+++ app/checkout/page.tsx
@@ -0,0 +1,37 @@
+'use client'
+
+export default function checkout(){
+    console.log('executing');
+    return(
+
+        <div>
+           {/* spacer */}
+           <div className = "mt-20 ml-3 mr-3 flex flex-col">
+           
+           {/* header */}
+            <div className = "text-5xl mt-7 mb-7 ">
+                Your bag
+            </div>
+
+            <div className = "text-2xl mt-2 mb-2">
+               Added Events
+            </div>
+
+            {/* hr */}
+            <div className='h-[1px] w-full bg-void-500'/>
+
+           {/* added events */}
+
+
+           {/* total */}
+
+            
+            </div>
+        </div>
+             
+
+            
+        
+    )
+}
+
