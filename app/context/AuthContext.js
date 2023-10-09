33af917958ad871e0d91cfde0b32dc5793b37470
rohithc123 <rohithc.eec.cseb@gmail.com>
2023-10-09 11:17:54 +0530

    adding basic auth and adding provider

---
+++ app/context/AuthContext.js
@@ -0,0 +1,42 @@
+"use client"
+import { useContext ,  createContext , useState , useEffect} from "react"
+import {signInWithPopup , signOut, onAuthStateChanged, GoogleAuthProvider } from "firebase/auth"
+import { auth } from "../firebase" 
+
+const AuthContext = createContext()
+
+export const AuthContextProvider = ({children}) => {
+
+    const [ user, setUser ] = useState(null)
+
+     
+    // sign in
+    const googleSignIn = () => {
+        const provider = new GoogleAuthProvider()
+        signInWithPopup(auth,provider)
+     }
+
+    // sign out 
+    
+    const googleLogOut = () => {
+        signOut(auth)
+    }
+
+    useEffect( () => {
+        const unsubscribe = onAuthStateChanged(auth , (currentUser) =>{
+            setUser(currentUser)
+        })
+    return () => unsubscribe()
+    },[user])
+
+    return (
+        <AuthContext.Provider value={user,googleSignIn,}googleLogOut>
+            {children}
+        </AuthContext.Provider>
+
+    )
+}
+
+export const UserAuth = () => {
+    return useContext(AuthContext);
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import Navbar from './_components/navbar'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import Footer from './_components/footer'
+import { SessionProvider } from 'next-auth'
+import { AuthContextProvider  } from "./context/AuthContext"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,10 +21,54 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <AuthContextProvider>
         <Navbar />
         {children}
         <Footer />
+        </AuthContextProvider>
       </body>
     </html>
   )
 }
+
+// export default function App({ Component, pageProps }) {
+//   return (
+//      <SessionProvider session = {pageProps.session ? pageProps.session : {}}>
+//        <RootLayout>
+//          <Component {...pageProps} />
+//        </RootLayout>
+//      </SessionProvider>
+//   )
+//  }
+
+// import './globals.css'
+// import Navbar from './_components/navbar'
+// import type { Metadata } from 'next'
+// import { Inter } from 'next/font/google'
+// import Footer from './_components/footer'
+// import { SessionProvider } from 'next-auth/react'
+
+// const inter = Inter({ subsets: ['latin'] })
+
+// export const metadata: Metadata = {
+//   title: 'Kratos 23 | EEC',
+//   description: 'TODO description',
+// }
+
+// export default function RootLayout({
+//   children,
+// }: {
+//   children: React.ReactNode
+// }) {
+//   return (
+//     <SessionProvider>
+//       <html lang="en">
+//         <body className={inter.className}>
+//           <Navbar />
+//           {children}
+//           <Footer />
+//         </body>
+//       </html>
+//     </SessionProvider>
+//   )
+// }
