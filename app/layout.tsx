71d0d4160a9d663e16d6081dadd2bb675363f568
rohithc123 <rohithc.eec.cseb@gmail.com>
2023-10-10 11:41:56 +0530

    adding context

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import Navbar from './Navbar'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import Footer from './Footer'
+import { AuthContextProvider  } from "./context/AuthContext"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,9 +20,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+      <AuthContextProvider>
         <Navbar />
         {children}
         <Footer />
+      </AuthContextProvider>
       </body>
     </html>
   )
